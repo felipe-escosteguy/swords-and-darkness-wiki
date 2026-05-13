a24b57d41dcee9eb8045f43e9753420da3b7a4b0
felipe-escosteguy <felipe@bacamarte.art.br>
2026-05-13 16:55:58 -0300

    Font Update

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -23,6 +23,11 @@ function getSidebarItems(dir) {
 export default defineConfig({
   title: "Swords & Darkness",
   description: "Campaign setting and rules wiki",
+  head: [
+    ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
+    ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
+    ['link', { href: 'https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&family=EB+Garamond:ital,wght@0,400..800;1,400..800&display=swap', rel: 'stylesheet' }]
+  ],
   themeConfig: {
     search: {
       provider: 'local'
